347b23ac96082a2e93e1ea15880f1677601b12bf
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-05-26 10:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const sizeElement=document.createElement("h1");
-sizeElement.innerText=`Width: &{window.innerWidth} and Height: &{window.innerHeight}`;
-document.body.append(sizeElement);
+const sizeElement=document.getElementByName("h1");
+sizeElement.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+// document.body.append(sizeElement);
 
 window.onresize=function() {
-	sizeElement.innerText=`Width: &{window.innerWidth} and Height: &{window.innerHeight}`;
+	sizeElement.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
 }
